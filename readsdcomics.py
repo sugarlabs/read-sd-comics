91a787e6abaa58f9252192dd12256c8eed7737a3
Aniket21mathur <amathur@ec.iitr.ac.in>
2019-01-18 12:51:20 +0530

    Use default stop accelerator
    Stop accelerator was ctrl+shift+q, but now is ctrl+q.
    
    The default accelerator is defined in the toolkit, so there is no
    need to set it in the activity code.  It is defined by the stop button.

--- readsdcomics.py
+++ readsdcomics.py
@@ -284,7 +284,6 @@ class ReadSDComics(activity.Activity):
         separator.show()
 
         stop_button = StopButton(self)
-        stop_button.props.accelerator = '<Ctrl><Shift>Q'
         toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
 
